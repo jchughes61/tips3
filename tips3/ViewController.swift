70951677575e91c27cf5c8865fc23c7ef795cff8
Jacob Hughes <jacobhughes@jacobs-macbook-pro.local>
2015-12-09 11:27:06 -0600

    Initial Commit

---
+++ tips3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tips3
+//
+//  Created by Jacob Hughes on 12/9/15.
+//  Copyright © 2015 Jacob Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
